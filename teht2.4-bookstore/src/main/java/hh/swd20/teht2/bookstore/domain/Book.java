4a1c2f1202a9287563d9c087d3c2ff6cc4070f07
haapjuu <empty>
2020-02-12 22:21:45 +0200

    controller & book class created

--- teht2.4-bookstore/src/main/java/hh/swd20/teht2/bookstore/domain/Book.java
+++ teht2.4-bookstore/src/main/java/hh/swd20/teht2/bookstore/domain/Book.java
@@ -2,4 +2,47 @@ package hh.swd20.teht2.bookstore.domain;
 
 public class Book {
 
+	private String title;
+	private String author;
+	private Integer isbn;
+	private Double price;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public Integer getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(Integer isbn) {
+		this.isbn = isbn;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public Book(String title, String author, Integer isbn, Double price) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.isbn = isbn;
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		return "Book [title=" + title + ", author=" + author + ", isbn=" + isbn + ", price=" + price + "]";
+	}
+	public Book() {
+		super();
+	}
 }
